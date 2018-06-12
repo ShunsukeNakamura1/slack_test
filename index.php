9579467ca8762e994f7de91b1fe745522a951562
ShunsukeNakamura1 <shushushu9999@gmail.com>
2018-06-13 04:25:12 +0900

    test

--- index.php
+++ index.php
@@ -17,37 +17,20 @@ $type = $json->type;
 echo "HTTP 200 OK";
 echo "Content-type: text/plain";
 echo $challenge;*/
-/*
-$url = 'https://slack.com/api/chat.postMessage';
 
-$data = array(
-  'token' => $json->token,
-  'channel' => $json->event->channel,
-  'text' => $json->event->text
-);
-$content = http_build_query($data);
-$options = array('http' => array(
-    'method' => 'POST',
-    'content' => $content
-));
-$result = file_get_contents($url, false, stream_context_create($options));
-error_log("--------");
-error_log($result);
-error_log("--------");
-*/
+$data = $json->event->text;
+$replymessage="";
+if(preg_match('/*@Test*/', $data)){
+    if(preg_match('/*Hello*/', $data)){
+        $replymessage = "Hello!\nHello";
+    }
+}
 
-$data = array(
-  'token' => $json->token,
-  'channel' => $json->event->channel,
-  'text' => $json->event->text
-);
-$data_json = json_encode($data);
 $ch = curl_init();
-//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-//curl_setopt($ch, CURLOPT_POSTFIELDS, $data_json);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL, 'https://slack.com/api/chat.postMessage?token='.'xoxp-367016455926-366262831957-379810825924-517aca11f1c617ab2125d0a15e00ad34'.'&channel='.$json->event->channel.'&text='.$json->event->text);
+$url = 'https://slack.com/api/chat.postMessage?token='.getenv('OAuthAccessToken').'&channel='.$json->event->channel.'&text='.$replymessage;
+curl_setopt($ch, CURLOPT_URL, $url);
 $result=curl_exec($ch);
 error_log("--------");
 error_log($result);
