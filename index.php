b75bfd9e7682270e80cc73a648b69fe0bdf1731e
ShunsukeNakamura1 <shushushu9999@gmail.com>
2018-06-13 03:49:58 +0900

    test

--- index.php
+++ index.php
@@ -17,10 +17,11 @@ $type = $json->type;
 echo "HTTP 200 OK";
 echo "Content-type: text/plain";
 echo $challenge;*/
-$url = 'https://slack.com/api/chat.postMessage?token='.$json->token;
+/*
+$url = 'https://slack.com/api/chat.postMessage';
 
 $data = array(
-  //'token' => $json->token,
+  'token' => $json->token,
   'channel' => $json->event->channel,
   'text' => $json->event->text
 );
@@ -33,3 +34,22 @@ $result = file_get_contents($url, false, stream_context_create($options));
 error_log("--------");
 error_log($result);
 error_log("--------");
+*/
+
+$data = array(
+  'token' => $json->token,
+  'channel' => $json->event->channel,
+  'text' => $json->event->text
+);
+$data_json = json_encode($data);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_json);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, 'https://slack.com/api/chat.postMessage');
+$result=curl_exec($ch);
+error_log("--------");
+error_log($result);
+error_log("--------");
+curl_close($ch);
